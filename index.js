c7200d752f4dd96b3b22d6d00b7633480bd60e9a
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-07 11:07:56 +0300

    Add new project

---
+++ index.js
@@ -0,0 +1,18 @@
+let a = prompt('Введите первую переменную', a);
+let b = prompt('Введите вторую переменную', b);
+
+function sum(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
