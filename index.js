1276252c4f8b120a1e24fce131292eef18e1cc08
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-09 23:58:07 +0300

    Amendments

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let a = prompt('Введите первую переменную', a);
-let b = prompt('Введите вторую переменную', b);
+let a = prompt('Введите первую переменную', 'a');
+let b = prompt('Введите вторую переменную', 'b');
 
 function sum(a, b) {
-    return a + b;
+    return + a + + b;
 }
+let result1 = sum(a, b);
+alert(result1);
 
 function subtraction(a, b) {
     return a - b;
 }
+let result2 = subtraction(a, b);
+alert(result2);
 
 function multiplication(a, b) {
     return a * b;
 }
 
+let result3 = multiplication(a, b);
+alert(result3);
+
 function division(a, b) {
     return a / b;
 }
+let result4 = division(a, b);
+alert(result4);
