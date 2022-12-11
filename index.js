71f3fa01571d56bf9c803bc35015130364002bc9
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-11 21:38:37 +0300

    Add amendments

--- index.js
+++ index.js
@@ -5,23 +5,31 @@ function sum(a, b) {
     return + a + + b;
 }
 let result1 = sum(a, b);
-alert(result1);
+function showSumResult() {
+    alert(result1);
+}
 
 function subtraction(a, b) {
     return a - b;
 }
 let result2 = subtraction(a, b);
-alert(result2);
+function showSubtractionResult() {
+    alert(result2);
+}
 
 function multiplication(a, b) {
     return a * b;
 }
 
 let result3 = multiplication(a, b);
-alert(result3);
+function showMultiplicationResult() {
+    alert(result3);
+}
 
 function division(a, b) {
     return a / b;
 }
 let result4 = division(a, b);
-alert(result4);
+function showDivisionResult() {
+    alert(result4);
+}
