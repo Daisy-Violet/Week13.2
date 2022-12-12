03df67b6147cce25e9fb618aa52fb156974c06d4
Daisy-Violet <a.laska.yang@mail.ru>
2022-12-13 01:11:43 +0300

    Amended

--- index.js
+++ index.js
@@ -2,34 +2,17 @@ let a = prompt('Введите первую переменную', 'a');
 let b = prompt('Введите вторую переменную', 'b');
 
 function sum(a, b) {
-    return + a + + b;
-}
-let result1 = sum(a, b);
-function showSumResult() {
-    alert(result1);
+    alert(`${a + b}`);
 }
 
 function subtraction(a, b) {
-    return a - b;
-}
-let result2 = subtraction(a, b);
-function showSubtractionResult() {
-    alert(result2);
+    alert(`${a - b}`);
 }
 
 function multiplication(a, b) {
-    return a * b;
-}
-
-let result3 = multiplication(a, b);
-function showMultiplicationResult() {
-    alert(result3);
+    alert(`${a * b}`);
 }
 
 function division(a, b) {
-    return a / b;
-}
-let result4 = division(a, b);
-function showDivisionResult() {
-    alert(result4);
+    alert(`${a / b}`);
 }
